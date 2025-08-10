f71a6aa4f59ce26b1c675e00dee712fb8c16b1d8
Saksham <151442281+sekshem@users.noreply.github.com>
2025-08-10 15:29:07 +0530

    Time: 7 ms (89.65%) | Memory: 18.6 MB (14.18%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -3,19 +3,16 @@ class Solution:
         currProd = 1
         maxProd = nums[0]
         
-        # Forward pass
         for R in range(len(nums)):
             currProd = currProd * nums[R]
-            maxProd = max(maxProd, currProd)
-            if currProd == 0:  # Reset after zero
+            maxProd = max(currProd, maxProd)
+            if currProd == 0:
                 currProd = 1
         
-        # Backward pass to handle cases where optimal subarray excludes negatives
         currProd = 1
-        for L in range(len(nums) - 1, -1, -1):
+        for L in range(len(nums)-1,-1,-1):
             currProd = currProd * nums[L]
-            maxProd = max(maxProd, currProd)
-            if currProd == 0:  # Reset after zero
+            maxProd = max(currProd, maxProd)
+            if currProd == 0:
                 currProd = 1
-                
         return maxProd
