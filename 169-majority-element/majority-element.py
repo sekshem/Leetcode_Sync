2be04bf2b7e75138ffb9178ec471585d6eae189c
Saksham <151442281+sekshem@users.noreply.github.com>
2025-08-10 19:40:00 +0530

    Time: 15 ms (14.24%) | Memory: 19.2 MB (97.56%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,4 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        count_map = {}
+        n = len(nums)
+    
+    # Count frequencies
+        for num in nums:
+            count_map[num] = count_map.get(num, 0) + 1
+        
+        # Early return optimization: if we already found majority
+            if count_map[num] > n // 2:
+                return num
+    
+        return None
