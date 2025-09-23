03f3e18268214eb341697a3d8dc4eccd37294a22
Saksham <151442281+sekshem@users.noreply.github.com>
2025-09-23 14:58:09 +0530

    Time: 17 ms (69.25%) | Memory: 17.7 MB (97.83%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -6,27 +6,21 @@ class Solution:
         s1_count = {}
         s2_count = {}
 
-        # Initialize counts for s1 and first window in s2
         for i in range(len(s1)):
             s1_count[s1[i]] = 1 + s1_count.get(s1[i], 0)
             s2_count[s2[i]] = 1 + s2_count.get(s2[i], 0)
-
+        
         if s1_count == s2_count:
             return True
-
-        # Sliding window
-        l = 0
-        for r in range(len(s1), len(s2)):
-            # include new character
+                
+        l = 0 
+        for r in range(len(s1),len(s2)):
             s2_count[s2[r]] = 1 + s2_count.get(s2[r], 0)
-
-            # remove leftmost character
             s2_count[s2[l]] -= 1
+
             if s2_count[s2[l]] == 0:
                 del s2_count[s2[l]]
-            l += 1
-
+            l+=1
             if s1_count == s2_count:
-                return True
-
-        return False
+                return True 
+        return False        
