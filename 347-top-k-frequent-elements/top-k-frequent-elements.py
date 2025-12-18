1d2828dddb8b927e4622bfda05e4d1be5dd2c85a
Saksham <151442281+sekshem@users.noreply.github.com>
2025-12-19 01:06:43 +0530

    Time: 7 ms (51.28%) | Memory: 20.6 MB (99.98%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        mp = {}
+        for i in range(len(nums)):
+            mp[nums[i]] = mp.get(nums[i],0) + 1
+        a = sorted(mp.keys(), key= lambda x : mp[x], reverse = True )
+        b = a[:k]
+        return b
+             
+        
