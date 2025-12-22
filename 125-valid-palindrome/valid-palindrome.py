1aeed230587efc9596cd713a9e8479782f68e005
Saksham <151442281+sekshem@users.noreply.github.com>
2025-12-22 16:56:31 +0530

    Time: 11 ms (36.40%) | Memory: 22.5 MB (18.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -3,11 +3,10 @@ class Solution:
         s = ''.join(c.lower() for c in s if c.isalnum())
         l = 0
         r = len(s) - 1
-        while l < r: 
+        while l < r:
             if s[l] != s[r]:
                 return False
-            else: 
-                l+=1
-                r-=1
+            l+=1
+            r-=1
         return True
-       
+              
