2cecb12d5c2d0cdf791403ced7e0fcc8268a0637
Saksham <151442281+sekshem@users.noreply.github.com>
2025-12-29 02:12:13 +0530

    Time: 6 ms (48.35%) | Memory: 24.3 MB (98.64%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,10 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k = k % n 
-        if k == 0 : return
-    
-        temp = nums[-k:]
-        nums[k:] = nums[:-k]
-        nums[:k] = temp
+        k = k % len(nums)
+        def reverse(l,r):    
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l+=1
+                r-=1
+        reverse(0,len(nums)-1)
+        reverse(0,k-1)
+        reverse(k, len(nums)-1)
